5d095a0cb983f312a61f88fffd3c5acbc2100fd5
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2021-12-27 14:40:21 -0300

    [style]: prop drill digit color

--- src/components/elements/SevenSegmentsDisplay/SevenSegmentsDisplay.jsx
+++ src/components/elements/SevenSegmentsDisplay/SevenSegmentsDisplay.jsx
@@ -3,7 +3,7 @@ import Digit from './Digit'
 import classes from './seve-segment-display.module.css'
 
 function SevenSegmentsDisplay(props) {
-    const { value } = props
+    const { value, color } = props
 
     return (
         <div className={classes.root}>
@@ -12,7 +12,11 @@ function SevenSegmentsDisplay(props) {
                     .toString()
                     .split('')
                     .map((d, index) =>
-                        <Digit value={parseInt(d)} key={index} />)
+                        <Digit
+                            color={color}
+                            value={parseInt(d)}
+                            key={index}
+                        />)
             }
         </div>
     )
