aaa64dbc3b8c6b316a2ae66ef680916ac0a9510b
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2021-12-27 14:41:11 -0300

    [feat]: integrates views with redux logic

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchNumber } from '@/redux/features/game/gameSlice'
+
 import {
     Page,
     SevenSegmentsDisplay,
@@ -8,14 +12,44 @@ import Title from './Title'
 import classes from './home.module.css'
 
 function Home() {
+    const game = useSelector(state => state.game)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchNumber())
+    }, [])
+
+    console.log(game)
+
     return (
         <Page
             head={<Title />}
             bottom={<AnswerForm />}
         >
             <div className={classes.content}>
-                <h4>é menor</h4>
-                <SevenSegmentsDisplay value={123} />
+                <h4
+                    style={{
+                        color: game.error ? 'var(--danger)' :
+                            game.won ? 'var(--success)' :
+                                'black'
+                    }}
+                >
+                    {
+                        game.error ? 'Erro' :
+                            game.loading ? 'Carregando' :
+                                game.won ? 'Você acertou!!!' :
+                                    game.value > game.throb ? 'É maior' :
+                                        'É menor'
+                    }
+                </h4>
+                <SevenSegmentsDisplay
+                    color={
+                        game.error ? 'var(--danger)' :
+                            game.won ? 'var(--success)' :
+                                'black'
+                    }
+                    value={game.error ? game.error.code : game.throb}
+                />
             </div>
         </Page>
     )
