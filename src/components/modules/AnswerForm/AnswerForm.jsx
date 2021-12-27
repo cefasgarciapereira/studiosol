b7fac0db1ce3e16d6ef76eb8190a3a084a690c96
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2021-12-27 14:40:34 -0300

    [feat]: handle game states

--- src/components/modules/AnswerForm/AnswerForm.jsx
+++ src/components/modules/AnswerForm/AnswerForm.jsx
@@ -1,12 +1,41 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchNumber, submit, reset } from '@/redux/features/game/gameSlice'
+
 import classes from './answer-form.module.css'
 
 function AnswerForm() {
+    const game = useSelector(state => state.game)
+    const dispatch = useDispatch()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        dispatch(submit(formData.get('number')))
+    }
+
+    function playAgain() {
+        dispatch(reset())
+        dispatch(fetchNumber())
+    }
+
     return (
         <div className={classes.root}>
-            <button>nova partida</button>
-            <form>
-                <input placeholder='Digite o palpite' type="numeric" />
-                <input type="submit" />
+            {
+                (game.won || game.error) &&
+                <button onClick={playAgain}>nova partida</button>
+            }
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder='Digite o palpite'
+                    type="numeric"
+                    name="number"
+                    maxLength={3}
+                    disabled={game.error || game.won}
+                />
+                <input
+                    type="submit"
+                    disabled={game.error || game.won}
+                />
             </form>
         </div>
     )
